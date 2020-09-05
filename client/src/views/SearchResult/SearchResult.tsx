b2c0fc2973dacaded7bd4f2ade4f497ad4953e6f
sidmonta <sidmonta.15@gmail.com>
2020-09-05 21:18:28 +0200

    Implement SearchResult page

--- client/src/views/SearchResult/SearchResult.tsx
+++ client/src/views/SearchResult/SearchResult.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from '@reach/router'
 
-import Title from '../../components/title/title'
+import WoodBookcase from '../../components/woodbookcase/WoodBookcase'
+import BookList from '../../components/booklist/BookList'
+import { useWebSocket, useWSData } from '../../context/websocket'
 
 export default function SearchResult({ path }: { path: string }) {
   const { query } = useParams()
+  const webSocketClient = useWebSocket()
+  const [books, setBook] = useWSData<string>('BOOKSEARCH')
 
+  useEffect(() => {
+    if (query) {
+      setBook([])
+      webSocketClient.emit('BOOKSEARCH', { query })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
   return (
-    <div>
-      <Title> {query} </Title>
+    <div className="page-container">
+      <WoodBookcase title={query || ''}>
+        <div className="wood-book">
+          <BookList books={books} />
+        </div>
+      </WoodBookcase>
     </div>
   )
 }
